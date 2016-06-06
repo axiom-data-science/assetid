86dab975001fa6136d88528c0308117984304055
Ryan <ryan@axiomdatascience.com>
2016-06-06 15:47:40 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -1,55 +1,28 @@
 #!/bin/bash
 
 NAME=assetid
-ORG=axiom-data-science
 
 if [ $# -eq 0 ]; then
     echo "No version specified, exiting"
     exit 1
 fi
 
+VERSION=$1
+VERSIONSTR="v${1}"
+
 # Set version to release
-sed -i "s/^__version__ = .*/__version__ = \"$1\"/" pyaxiom/__init__.py
-sed -i "s/version: .*/version: \"$1\"/" conda-recipe/meta.yaml
-sed -i "s/git_rev: .*/git_rev: $1/" conda-recipe/meta.yaml
+echo $VERSION > VERSION
+sed -i "s/^__version__ = .*/__version__ = \"$VERSION\"/" "${NAME}/__init__.py"
+
+git add "${NAME}/__init__.py"
 
 # Commit release
-git add pyaxiom/__init__.py
-git add conda-recipe/meta.yaml
-git commit -m "Release $1"
-
-# Tag
-git tag $1
+git commit -m "Release $VERSIONSTR"
+git tag $VERSIONSTR
 
 # Release on PyPi
 python setup.py sdist upload
 
 # Push to Git
-git push --tags origin master
-
-# Build 2.7
-conda build -c ioos --python 2.7 conda-recipe
-PACKAGE_PATH=`ls ~/miniconda3/conda-bld/**/$NAME*-$1-py27*.tar.bz2`
-conda convert --platform all $PACKAGE_PATH -o conda-recipe/py27
-for f in conda-recipe/py27/**/$NAME*; do
-    anaconda upload -u $ORG --force $f
-done
-rm -r conda-recipe/py27
-
-# Build 3.4
-conda build -c ioos --python 3.4 conda-recipe
-PACKAGE_PATH=`ls ~/miniconda3/conda-bld/**/$NAME*-$1-py34*.tar.bz2`
-conda convert --platform all $PACKAGE_PATH -o conda-recipe/py34
-for f in conda-recipe/py34/**/$NAME*; do
-    anaconda upload -u $ORG --force $f
-done
-rm -r conda-recipe/py34
-
-# Build 3.5
-conda build -c ioos --python 3.5 conda-recipe
-PACKAGE_PATH=`ls ~/miniconda3/conda-bld/**/$NAME*-$1-py35*.tar.bz2`
-conda convert --platform all $PACKAGE_PATH -o conda-recipe/py35
-for f in conda-recipe/py35/**/$NAME*; do
-    anaconda upload -u $ORG --force $f
-done
-rm -r conda-recipe/py35
+git push --tags
+git push
