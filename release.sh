a266f03f27ee101b6f978f3dd62e4dfcd7808773
Ryan <ryan@axiomdatascience.com>
2016-06-07 09:13:17 -0400

    Update release script
    Changes based on the comments on 86dab97:
    - Use versions without "v" prefix
    - Add VERSION file to git

--- release.sh
+++ release.sh
@@ -8,17 +8,17 @@ if [ $# -eq 0 ]; then
 fi
 
 VERSION=$1
-VERSIONSTR="v${1}"
 
 # Set version to release
 echo $VERSION > VERSION
 sed -i "s/^__version__ = .*/__version__ = \"$VERSION\"/" "${NAME}/__init__.py"
 
+git add VERSION
 git add "${NAME}/__init__.py"
 
 # Commit release
-git commit -m "Release $VERSIONSTR"
-git tag $VERSIONSTR
+git commit -m "Release $VERSION"
+git tag $VERSION
 
 # Release on PyPi
 python setup.py sdist upload
